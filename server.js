4c303add0c8d0354d407ea6f92614dde369dcf6c
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-09 23:45:37 +0530

    added necessary changes to host app on heroku

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 // create the app
 const app = express()
+const PORT = process.env.PORT || 3000
 
 // adds funationality to your app
 app.use(express.static('public')) // will expose the folder 'public' to the web server
 
-app.listen(3000, () => {
-  console.log('Express server is running on port 3000')
+// Re-directing https requests to http requests, as the API we are using only gives out http data
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next()
+  } else {
+    response.redirect('http://' + req.hostname + req.url)
+  }
+})
+
+app.listen(PORT, () => {
+  console.log('Express server is running on port ' + PORT)
 })
