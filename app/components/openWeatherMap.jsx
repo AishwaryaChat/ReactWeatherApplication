052034b4fa93b71421383a4e7ee0eab8bc7700cd
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-10 10:41:04 +0530

    changed the OpenWeatherMap file

--- app/components/openWeatherMap.jsx
+++ app/components/openWeatherMap.jsx
@@ -7,13 +7,13 @@ module.exports = {
     let encodedLocation = encodeURIComponent(location) // to wash off spaces and other characters
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`
     return axios.get(requestUrl)
-    .then((res) => {
-      if(res.data.list.length === 0){
-        throw new Error(res.data.message)
+    .then((response) => {
+      if(respone.data.list.length === 0){
+        throw new Error(response.data.message)
       } else {
-        return res.data.list[0].main.temp
+        return response.data.list[0].main.temp
       }
     })
-    .catch((err) => {throw new Error(res.data.message)})
+    .catch((err) => {throw new Error(response.data.message)})
   }
 }
